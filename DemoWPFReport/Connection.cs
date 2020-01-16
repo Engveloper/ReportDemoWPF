f62fcc1fe1d7625aa112ce458577c27da552e999
Engveloper <engelsbaca@spartansv.net>
2020-01-16 10:21:59 -0600

    Soy el primer commit :v

---
+++ DemoWPFReport/Connection.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoWPFReport
+{
+    public static class Connection
+    {
+        private static SqlConnection getConnection()
+        {
+            return new SqlConnection("Data Source=.;Initial Catalog=reportdb;Integrated Security=True");
+        }
+
+        public static DataSet getDataSet(int id_factura)
+        {
+            using(SqlConnection conn = getConnection())
+            {
+                conn.Open();
+                DataSet ds = new DataSet();
+                SqlDataAdapter da = new SqlDataAdapter();
+                SqlCommand cmd = conn.CreateCommand();
+                cmd.CommandText = "exec calcular_factura " + id_factura;
+                da.SelectCommand = cmd;
+                da.Fill(ds, "hola");
+                cmd.CommandText = "exec lineas_factura " + id_factura;
+                da.SelectCommand = cmd;
+                da.Fill(ds, "hola2");
+                conn.Close();
+                return ds;
+            }
+        } 
+    }
+}
---
+++ DemoWPFReport/MainWindow.xaml.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.Reporting.WinForms;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace DemoWPFReport
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonF_Click(object sender, RoutedEventArgs e)
+        {
+
+            ReportDataSource rds = new ReportDataSource("InvoiceDataSet");
+            rds.Value = Connection.getDataSet(1).Tables[0];
+
+            ReportDataSource rds2 = new ReportDataSource("ProductsDataSet");
+            rds2.Value = Connection.getDataSet(1).Tables[1];
+
+            reportViewer1.LocalReport.ReportPath = "../../InvoiceReport.rdlc";
+            reportViewer1.LocalReport.Refresh();
+            reportViewer1.LocalReport.DataSources.Add(rds);
+            reportViewer1.LocalReport.DataSources.Add(rds2);
+
+            
+            reportViewer1.RefreshReport();
+        }
+    }
+}
